253139f1e9833f29c10ce61907b20cb0dc2abd04
Luu Hung <hungluu344@gmail.com>
2024-10-11 00:17:03 +0700

    Added Product controller

---
+++ Winery/Models/Products.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Winery.Models
+{
+    public class Products
+    {
+        #region Properties
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public double ProductPrice { get; set; }
+        public string ProductCapacity { get; set; }
+        #endregion
+
+
+    }
+}
