1600d4da72ee66d872272c18c60b22a84e1fe025
Luu Hung <hungluu344@gmail.com>
2024-10-18 03:08:07 +0700

    Added row New Arrivals in Home

--- Winery/Controllers/HomeController.cs
+++ Winery/Controllers/HomeController.cs
@@ -1,17 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using Winery.Models;
 
 namespace Winery.Controllers
 {
     public class HomeController : Controller
     {
+
+        private WineryEntities2 db = new WineryEntities2();
+
+        private List<T> GetRandomItems<T>(List<T> items, int count)
+        {
+            return items.OrderBy(x => Guid.NewGuid()).Take(count).ToList();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var products = db.Product.Include(p => p.Category).ToList();
+
+            // Select 5 random products
+            var randomProducts = GetRandomItems(products, 5);
+
+            // Pass to the view
+            return View(randomProducts);
         }
+
+
 
         public ActionResult About()
         {
