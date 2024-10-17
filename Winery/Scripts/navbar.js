2b15107851876bf2643a021547959e8c28c1dd67
Luu Hung <hungluu344@gmail.com>
2024-10-17 13:55:40 +0700

    Update nav bar, layout

---
+++ Winery/Scripts/navbar.js
@@ -0,0 +1,8 @@
+﻿document.addEventListener('scroll', () => {
+    const header = document.getElementById('navbar-content-main');
+    if (window.scrollY > 0) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+})
