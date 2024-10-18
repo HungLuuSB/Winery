50420b784edf911dfc06c7a8a5498fe04454151f
Luu Hung <hungluu344@gmail.com>
2024-10-19 04:00:46 +0700

    Login session logic

--- Winery/Controllers/LoginController.cs
+++ Winery/Controllers/LoginController.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Winery.Models;
 
 namespace Winery.Controllers
 {
     public class LoginController : Controller
     {
+        private WineryEntities2 db = new WineryEntities2();
         // GET: Login
         [HttpPost]
-        public ActionResult Index()
+        public ActionResult Login(User User)
         {
+            var user = db.User.Where(x => x.Email.Equals(User.Email) && x.Password.Equals(User.Password)).FirstOrDefault();
+            if (user != null) {
+                Session["username"] = user.Username.ToString();
+                return RedirectToAction("Index", "Home");
+            }
             return View();
         }
     }
--- Winery/Ultilities/WineryUtilities.cs
+++ Winery/Ultilities/WineryUtilities.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Winery.Models;
 
 namespace Winery.Ultilities
 {
     public static class WineryUtilities
     {
+        private static WineryEntities2 db = new WineryEntities2();
         public static string ParseItemNameIntoPNGFileName(string name)
         {
             string temp = name.ToLower().Replace(' ', '_') + ".png";
@@ -17,6 +19,15 @@ namespace Winery.Ultilities
             if (year == null)
                 return 0;
             return DateTime.Now.Year - year;
+        }
+        public static int? GetUserIDFromName(string username) 
+        {
+            var user = db.User.Where(x => x.Username.Equals(username)).FirstOrDefault();
+            if (user != null)
+            {
+                return user.UserID;
+            }
+            return null;
         }
     }
 }
