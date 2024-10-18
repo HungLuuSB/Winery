9830597a74a77c36c3d55d4a18e59d4dfac4609e
Luu Hung <hungluu344@gmail.com>
2024-10-19 00:19:30 +0700

    cropped footer-img.jpg added controller Login

--- Winery/Controllers/LoginController.cs
+++ Winery/Controllers/LoginController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Winery.Models;
 
 namespace Winery.Controllers
 {
     public class LoginController : Controller
     {
+        private WineryEntities2 db = new WineryEntities2();
         // GET: Login
         public ActionResult Index()
         {
