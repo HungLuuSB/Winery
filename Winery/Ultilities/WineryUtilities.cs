1515fba5a3a0eb40693986b0fecc8ba856fe3845
Luu Hung <hungluu344@gmail.com>
2024-10-18 21:14:25 +0700

    Add Utilities folder to contain frequently used methods

---
+++ Winery/Ultilities/WineryUtilities.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Winery.Ultilities
+{
+    public static class WineryUtilities
+    {
+        public static string ParseItemNameIntoPNGFileName(string name)
+        {
+            string temp = name.ToLower().Replace(' ', '_') + ".png";
+            return temp;
+        }
+        public static int? GetAgingYearFromNow(int? year)
+        {
+            if (year == null)
+                return 0;
+            return DateTime.Now.Year - year;
+        }
+    }
+}
