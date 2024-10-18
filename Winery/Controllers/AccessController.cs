65eadba881dc31ce59e102acb0c1603853530703
Luu Hung <hungluu344@gmail.com>
2024-10-19 02:25:07 +0700

    Added basic controls for Access/Login

---
+++ Winery/Controllers/AccessController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Winery.Controllers
+{
+    public class AccessController : Controller
+    {
+        // GET: Access
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
