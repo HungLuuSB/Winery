1eeea7fb85c98212e9d32c64af299e180e4e2fec
Luu Hung <hungluu344@gmail.com>
2024-10-30 05:29:21 +0700

    Added CartService

---
+++ Winery/Services/CartService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Winery.Models;
+
+namespace Winery.Services
+{
+    public static class CartService
+    {
+        public static List<Product> Cart = new List<Product>();
+
+        public static void LoadCartOfUser()
+        {
+            if (UserSessionService.IsUserLoggedIn())
+            {
+                Cart.Clear();
+            }
+        }
+    }
+}
