141ab55a5316469674faf3014c0a7c906ccf9164
Luu Hung <hungluu344@gmail.com>
2024-10-30 06:33:11 +0700

    Changed somelayout

---
+++ Winery/Services/CartService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Winery.Models;
+using Winery.Services;
+
+namespace Winery.Services
+{
+    public static class CartService
+    {
+        private static List<Order> _orders = new List<Order>();
+        public static List<Order> Orders
+        {
+            get
+            {
+                return _orders;
+            }
+        }
+        
+
+    }
+}
