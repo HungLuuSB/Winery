4de2e355078923b379aed4028e62b6c22e6879bb
Huy Phan <qhuy122@gmail.com>
2024-11-11 21:37:14 +0700

    quantity

---
+++ Winery/Scripts/product-details.js
@@ -0,0 +1,10 @@
+﻿function increaseQuantity() {
+    var input = document.getElementById("quantity").value;
+    input += 1;
+}
+function deccreaseQuantity() {
+    var input = document.getElementById("quantity").value;
+    if (input > 1) {
+        input -= 1;
+    }
+}
