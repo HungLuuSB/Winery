75f71ce90af8a5628a6eaa315c3a913fea58c723
Luu Hung <hungluu344@gmail.com>
2024-11-12 22:33:31 +0700

    Test

--- Winery/App_Start/RouteConfig.cs
+++ Winery/App_Start/RouteConfig.cs
@@ -12,8 +12,6 @@ namespace Winery
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
